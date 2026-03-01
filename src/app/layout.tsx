361c1165bcc23555afb6e0d05692a849efc2968d
NoIdea2001 <saumyabhaintwal@gmail.com>
2026-03-01 18:15:18 +0530

    feat(analytics): integrate Vercel Analytics and Speed Insights
    - Added @vercel/analytics
    - Added @vercel/speed-insights
    - Integrated Analytics and SpeedInsights into root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Providers } from "@/components/providers";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +32,8 @@ export default function RootLayout({
       >
         <Providers>{children}</Providers>
       </body>
+      <Analytics/>
+      <SpeedInsights/>
     </html>
   );
 }
